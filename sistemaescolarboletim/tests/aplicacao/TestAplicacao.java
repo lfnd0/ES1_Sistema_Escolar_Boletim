fd63b4003d43e4acffddb396a8967422bd04303e
Luiz Fernando <luiz-fsilva37@hotmail.com>
2019-04-13 10:25:32 -0300

    Atualização do projeto

--- sistemaescolarboletim/tests/aplicacao/TestAplicacao.java
+++ sistemaescolarboletim/tests/aplicacao/TestAplicacao.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import br.seb.aplicacao.Aplicacao;
 import junit.framework.TestCase;
 
-public class TestAplicacaoDAOBDR extends TestCase {
+public class TestAplicacao extends TestCase {
 	
 	private Aplicacao aplicacao = new Aplicacao();
 	
